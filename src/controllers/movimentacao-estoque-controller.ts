1234834d1fbcf464b0ea3cb3eef03bcee441458e
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-10-12 13:27:46 -0300

    CRUD movimentação de estoque

--- src/controllers/movimentacao-estoque-controller.ts
+++ src/controllers/movimentacao-estoque-controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import {
+  createMovimentacaoEstoque,
+  deleteMovimentacaoEstoque,
   getAllMovimentacoesEstoque,
   getMovimentacaoEstoqueById,
+  updateMovimentacaoEstoque,
 } from "../models/movimentacao-estoque";
 
 // Consultar todas as movimentações de estoque
@@ -33,5 +36,77 @@ export const getMovimentacaoEstoque = async (
     }
   } catch (error) {
     res.status(500).json({ message: "Erro ao buscar movimentação" });
+  }
+};
+
+// Criar nova movimentação de estoque
+export const createNewMovimentacaoEstoque = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const {
+      movimento_produto_id,
+      movimento_quantidade,
+      movimento_tipo,
+      movimento_data,
+      movimento_observacao,
+    } = req.body;
+    const newMovimentacaoEstoque = await createMovimentacaoEstoque(
+      movimento_produto_id,
+      movimento_quantidade,
+      movimento_tipo,
+      movimento_data,
+      movimento_observacao
+    );
+    res.status(201).json(newMovimentacaoEstoque);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao criar movimentação" });
+  }
+};
+
+// Atualizar uma movimentação de estoque específica
+export const updateExistingMovimentacaoEstoque = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const movimento_id = Number(req.params.movimento_id);
+    const {
+      movimento_produto_id,
+      movimento_quantidade,
+      movimento_tipo,
+      movimento_data,
+      movimento_observacao,
+    } = req.body;
+    const updatedMovimentacaoEstoque = await updateMovimentacaoEstoque(
+      movimento_id,
+      movimento_produto_id,
+      movimento_quantidade,
+      movimento_tipo,
+      movimento_data,
+      movimento_observacao
+    );
+    if (updatedMovimentacaoEstoque) {
+      res.json(updatedMovimentacaoEstoque);
+    } else {
+      res.status(404).json({ message: "Movimentação não encontrada" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao atualizar movimentação" });
+  }
+};
+
+// Deletar uma movimentação de estoque específica
+export const deleteExistingMovimentacaoEstoque = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const movimento_id = Number(req.params.movimento_id);
+    await deleteMovimentacaoEstoque(movimento_id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao deletar movimentação" });
   }
 };
--- src/routes/movimentacao-estoque-routes.ts
+++ src/routes/movimentacao-estoque-routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import {
+  createNewMovimentacaoEstoque,
+  deleteExistingMovimentacaoEstoque,
   getMovimentacaoEstoque,
   getMovimentacoesEstoque,
+  updateExistingMovimentacaoEstoque,
 } from "../controllers/movimentacao-estoque-controller";
 
 const router = Router();
 
 router.get("/movimento", getMovimentacoesEstoque); // Consultar todas as movimentações de estoque
 router.get("/movimento/:movimento_id", getMovimentacaoEstoque); // Consultar movimentação de estoque por ID
+router.post("/movimento", createNewMovimentacaoEstoque); // Criar nova movimentação de estoque
+router.put("/movimento/:movimento_id", updateExistingMovimentacaoEstoque); // Atualizar movimentação de estoque por ID
+router.delete("/movimento/:movimento_id", deleteExistingMovimentacaoEstoque); // Deletar movimentação de estoque por ID
 
 export default router;
