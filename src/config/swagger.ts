3a2422ce45ff71b718bd3c01cf463eecd8bb605f
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-10-13 00:25:31 -0300

    retirada de arquivos não mais necessários e bibliotecas não utilizadas no swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,39 +1,10 @@
-import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
 import swaggerDocument from "../swagger.json";
-import pedidoSchema from "../schemas/pedido-schema";
-
-const env = process.env.NODE_ENV || "development";
 
 // CDN CSS
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API do Sistema de Gestão de Estoques",
-      version: "1.0.0",
-      description:
-        "Documentação da API de Gestão de Estoques para uma hamburgueria",
-    },
-    servers: [
-      {
-        url: "https://controle-de-estoque-pi-2-api.vercel.app/v1",
-      },
-    ],
-    components: {
-      schemas: {
-        Pedido: pedidoSchema,
-      },
-    },
-  },
-  apis: env === "production" ? ["./dist/routes/*.js"] : ["./src/routes/*.ts"], // Diferentes caminhos para produção e desenvolvimento
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
   app.use(
